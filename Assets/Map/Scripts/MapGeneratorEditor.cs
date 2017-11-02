a3139028ccf2ab3390143f3864ed45e4c69b8294
Unknown <aapo.lempio@hotmail.fi>
2017-11-02 15:05:42 +0200

    Implement Map
    Add Map generation and map functionality

---
+++ Assets/Map/Scripts/MapGeneratorEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Maps
+{
+    [CustomEditor(typeof(MapGenerator))]
+    class MapGeneratorEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            MapGenerator generator = (MapGenerator)target;
+            if (GUILayout.Button("Generate Map"))
+            {
+                generator.GenerateMap();
+            }
+        }
+    }
+}
---
+++ Assets/Map/Scripts/Serialization/Bounds.cs
@@ -0,0 +1,27 @@
+﻿using System.Xml;
+using UnityEngine;
+
+namespace Maps
+{
+    public class Bounds
+    {
+        public float MinLat { get; private set; }
+        public float MaxLat { get; private set; }
+        public float MinLon { get; private set; }
+        public float MaxLon { get; private set; }
+
+        public Vector3 Center { get; private set; }
+
+        public Bounds(XmlNode node)
+        {
+            MinLat = XmlGetter.GetAttribute<float>("minlat", node.Attributes);
+            MaxLat = XmlGetter.GetAttribute<float>("maxlat", node.Attributes);
+            MinLon = XmlGetter.GetAttribute<float>("minlon", node.Attributes);
+            MaxLon = XmlGetter.GetAttribute<float>("maxlon", node.Attributes);
+
+            float x = (float)((MercatorProjection.lonToX(MaxLon) + MercatorProjection.lonToX(MinLon)) / 2);
+            float y = (float)((MercatorProjection.latToY(MaxLat) + MercatorProjection.latToY(MinLat)) / 2);
+            Center = new Vector3(x, 0, y);
+        }
+    }
+}
