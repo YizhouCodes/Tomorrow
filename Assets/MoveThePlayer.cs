ca886b0d19c569398404f900cf781b2d3a179617
Unknown <mahmoudabudaya97@gmail.com>
2017-11-07 16:28:35 +0200

    enable ui when the player approaches a hotspot

---
+++ Assets/MoveThePlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveThePlayer : MonoBehaviour {
+
+    public int speed;
+    void Start()
+    {
+     
+    }
+    void Update ()
+    {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+        Vector3 cur = transform.position;
+        cur.x += h * 0.3f;
+        cur.z += v * 0.3f;
+        transform.position = cur;
+        }
+}
