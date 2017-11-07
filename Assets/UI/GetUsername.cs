8038a5b232847e809997f5852284bba3e49dfe9a
zhou421513 <contact.yizhou@gmail.com>
2017-11-07 14:04:50 +0200

    Add GetUsername class
    Basic functionality

---
+++ Assets/UI/GetUsername.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; // Required when Using UI elements.
+
+public class GetUsername : MonoBehaviour
+{
+    public InputField mainInputField;
+
+    void Validate(InputField input)
+    {
+        if (input.text.Length > 0)
+        {
+            SetUsername su = new SetUsername();
+            su.call(input.text);
+        }
+       
+    }
+
+    public void Start()
+    {
+        //Adds a listener that invokes the "Validate" method when the player finishes editing the main input field.
+        //Passes the main input field into the method when "Validate" is invoked
+        mainInputField.onEndEdit.AddListener(delegate { Validate(mainInputField); });
+    }
+}
