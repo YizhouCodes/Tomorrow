983e4265aeb983a21bed0d6d01124939188f122c
Unknown <al422806@student.uta.fi>
2017-11-16 16:09:09 +0200

    Add Hotspot Generator

---
+++ Assets/Hotspots/Scripts/HotspotGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Maps;
+
+public class HotspotGenerator : MonoBehaviour {
+
+    public MapFunctionality map;
+    public GameObject hotspot;
+
+    //Create a single hotspot using the position vector3 and the name of the hotspot
+    private void createHotspot(Vector3 position, string name)
+    {
+        Object newHotspot = Instantiate(hotspot, position, Quaternion.identity);
+        newHotspot.name = name;
+    }
+
+    //Create all hotspots, even those outside of the map
+    public void createAllHotspots()
+    {
+        Hotspot[] hotspots = DataController.Instance.player_data.hotspots;
+        foreach(Hotspot hotspot in hotspots)
+        {
+            Vector3 position = map.MapPositionAt((float)hotspot.longitude, (float)hotspot.latitude);
+            createHotspot(position, hotspot.name);
+        }
+    }
+
+    private void Start()
+    {
+        createAllHotspots();
+    }
+}
