86deedce0b9ac003df86d3a92f185a56221e14a3
Unknown <aapo.lempio@hotmail.fi>
2017-11-22 16:34:33 +0200

    Add progressbar

---
+++ Assets/UI/BarLength.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarLength : MonoBehaviour {
+
+    void Update ()
+    {
+        transform.localScale = new Vector3(DataController.Instance.player_data.progress, 1, 1);
+	}
+}
