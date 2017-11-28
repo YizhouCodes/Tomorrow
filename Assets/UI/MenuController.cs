a6b46bf730bb331b79bee4a1c40aa07093b3cd9f
Arvin Jenabi <arvinje@gmail.com>
2017-11-28 23:36:51 +0200

    Add menu screen and controller

---
+++ Assets/UI/MenuController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+
+	public Text username;
+	public Text progress;
+
+	void Update () {
+		username.text = DataController.Instance.player_data.username;
+
+		string visited_count = DataController.Instance.player_data.visited_count.ToString();
+		string hotspots_count = DataController.Instance.player_data.hotspots_count.ToString();
+		progress.text = "You've visited " + visited_count + " out of " + hotspots_count + " hotspots";
+	}
+}
