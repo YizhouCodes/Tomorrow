8c8017c6adef086d55a3c3cb0eb472b2e12b5686
Md Imran Pavel <imran.pavel@gmail.com>
2017-12-13 14:33:14 +0200

    Resume Page Included

--- Assets/UI/StartScreensController.cs
+++ Assets/UI/StartScreensController.cs
@@ -6,12 +6,16 @@ using System.IO;
 public class StartScreensController : MonoBehaviour {
 
     public GameObject firstScreen;
+    public GameObject fourthScreen;
 
     public void call () {
         if (File.Exists(Application.persistentDataPath + "/playerData.dat"))
         {
             firstScreen = transform.GetChild(0).gameObject;
             firstScreen.SetActive(false);
+
+            fourthScreen = transform.GetChild(3).gameObject;
+            fourthScreen.SetActive(true);
         }
     }
 	
